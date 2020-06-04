d13e624bd685ad51ac545fb7cf6a50de05f01ce2
Pierre Mareschal <devleaks.be@gmail.com>
2020-06-04 14:17:10 +0200

    Added internal clock tick for simulation

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,9 +1,11 @@
 /*
  *  Stolen from https://codepen.io/dope/pen/KJYMZz
  */
+import PubSub from "pubsub-js"
+import moment from "moment"
+
 import { Subscriber } from "./Subscriber"
 import { deepExtend } from "./Utils/Utilities"
-import moment from "moment"
 
 
 import { CLOCK_MSG, SIMULATION_MSG } from "./Constant"
@@ -13,7 +15,18 @@ import "../assets/css/clock.css"
 const DEFAULTS = {
     refresh: 1, //seconds
     inc: 1, // seconds
-    log: false
+    log: false,
+    morethan: [ // minutes
+        1,
+        2,
+        5,
+        10,
+        15,
+        20,
+        30,
+        60,
+        120
+    ]
 }
 
 export class Clock extends Subscriber {
@@ -23,6 +36,7 @@ export class Clock extends Subscriber {
         this.options = deepExtend(DEFAULTS, options)
         this.elemid = elemid
         this.date = moment()
+        this.morethan = new Map()
         this.install()
         this.run()
     }
@@ -65,19 +79,46 @@ export class Clock extends Subscriber {
      * @param      {String}  data     The time as ISO 8601 formatted string.
      */
     update(msgtype, data) {
-        if(this.options.log) {
+        if (this.options.log) {
             console.log("Clock::updateClock", msgtype, data)
         }
-        switch(msgtype) {
+        switch (msgtype) {
             case CLOCK_MSG:
-            this.setClock(moment(data), moment.ISO_8601)
-            break
+                this.setClock(moment(data, moment.ISO_8601))
+                this.morethanEmit()
+                break
             case SIMULATION_MSG:
-            this.setClock(moment(data.timestamp), moment.ISO_8601)
-            this.setInc(data.speed)
+                this.setClock(moment(data.timestamp, moment.ISO_8601))
+                this.setInc(data.speed)
+                break
+            default:
+                console.log("Clock::update: Received default", msgtype)
+                break
         }
     }
 
+    /**
+     * Generates CLOCK_MSG.{DELAY} messages for Tile updates.
+     * Example: CLOCK_MSG.15 generates a message whenever 15 minutes elapsed since last generation of CLOCK_MSG.15.
+     */
+    morethanEmit() {
+        this.options.morethan.forEach((delay) => {
+            let lasttime = this.morethan.get(delay)
+            if (lasttime) {
+                const d = moment.duration(this.date.diff(lasttime)).asMinutes()
+
+                console.log("Clock::morethan", delay, d, d > delay, lasttime.toISOString(true), this.date.toISOString(true))
+                if (d > delay) {
+                    console.log("Clock::morethan: EMIT", CLOCK_MSG + "." + delay, this.date.toISOString(true))
+                    this.morethan.set(delay, this.date)
+                    PubSub.publish(CLOCK_MSG + "." + delay, this.date.toISOString(true))
+                }
+            } else {
+                console.log("Clock::morethan: setting", delay, this.date.toISOString(true))
+                this.morethan.set(delay, this.date)
+            }
+        })
+    }
 
     /**
      * Paint updated clock
@@ -88,11 +129,11 @@ export class Clock extends Subscriber {
 
         const seconds = this.date.seconds() * 6
         const minutes = this.date.minutes() * 6
-        const hours   = this.date.hours() * 30 + Math.floor( this.date.minutes() / 2 )
+        const hours = this.date.hours() * 30 + Math.floor(this.date.minutes() / 2)
 
         document.querySelector(".second").style.transform = `rotate(${seconds}deg)`
         document.querySelector(".minute").style.transform = `rotate(${minutes}deg)`
-        document.querySelector(".hour").style.transform   = `rotate(${hours}deg)`
+        document.querySelector(".hour").style.transform = `rotate(${hours}deg)`
     }
 
 
